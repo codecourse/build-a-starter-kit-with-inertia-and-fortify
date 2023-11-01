cc95a9e3395b00586cb0e2971e2a57e9681c0b60
Alex Garrett-Smith <alex@codecourse.com>
2023-11-01 08:50:52 +0000

    Episode 02

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <div>
+        Home
+    </div>
+
+    <Head title="Home" />
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import './bootstrap';
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+
+const appName = import.meta.env.VITE_APP_NAME
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+        .use(plugin)
+        .mount(el)
+    },
+})

29f09a8f0be507781c2175ca2c0bfeb93ea979a7
Alex Garrett-Smith <alex@codecourse.com>
2023-11-02 12:03:33 +0000

    Episode 31

--- resources/js/Pages/Account/Form/TwoFactorForm.vue
+++ resources/js/Pages/Account/Form/TwoFactorForm.vue
@@ -13,11 +13,39 @@ const enable = () => {
 const disable = () => {
     router.delete(route('two-factor.disable'))
 }
+
+const recoveryCodes = ref([])
+
+const fetchRecoveryCodes = () => {
+    axios.get(route('two-factor.recovery-codes')).then((response) => {
+        recoveryCodes.value = response.data
+    })
+}
 </script>
 
 <template>
     <div class="space-y-6">
+        <h2 class="font-bold text-gray-900 font-mono">Two factor authentication</h2>
+
+        <div v-if="$page.props.auth.user.two_factor_enabled" class="space-y-6">
+            <button v-on:click="disable" class="text-blue-500 font-semibold text-sm">Disable two factor authentication</button>
+
+            <div class="space-y-6">
+                <h2 class="font-bold text-gray-900 font-mono">Recovery codes</h2>
+
+                <div>
+                    <button class="text-blue-500 font-semibold text-sm" v-on:click="fetchRecoveryCodes" v-if="!recoveryCodes.length">Show recovery codes</button>
+
+                    <ul v-if="recoveryCodes.length" class="space-y-1">
+                        <li v-for="recoveryCode in recoveryCodes" :key="recoveryCode" class="text-sm">
+                            {{ recoveryCode }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
         <button v-on:click="enable" v-if="!$page.props.auth.user.two_factor_enabled" class="text-blue-500 font-semibold text-sm">Enable two factor authentication</button>
-        <button v-on:click="disable" v-if="$page.props.auth.user.two_factor_enabled" class="text-blue-500 font-semibold text-sm">Disable two factor authentication</button>
+
     </div>
 </template>

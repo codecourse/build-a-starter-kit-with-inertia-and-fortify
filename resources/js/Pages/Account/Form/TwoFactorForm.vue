085b2801d32e0cf50dcb18a8a42849d48da13662
Alex Garrett-Smith <alex@codecourse.com>
2023-11-02 10:50:10 +0000

    Episode 30

--- resources/js/Pages/Account/Form/TwoFactorForm.vue
+++ resources/js/Pages/Account/Form/TwoFactorForm.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { router } from '@inertiajs/vue3'
+import { ref } from 'vue'
 
 const enable = () => {
     router.post(route('two-factor.enable'), {}, {
@@ -8,10 +9,15 @@ const enable = () => {
         }
     })
 }
+
+const disable = () => {
+    router.delete(route('two-factor.disable'))
+}
 </script>
 
 <template>
-    <div>
+    <div class="space-y-6">
         <button v-on:click="enable" v-if="!$page.props.auth.user.two_factor_enabled" class="text-blue-500 font-semibold text-sm">Enable two factor authentication</button>
+        <button v-on:click="disable" v-if="$page.props.auth.user.two_factor_enabled" class="text-blue-500 font-semibold text-sm">Disable two factor authentication</button>
     </div>
 </template>
